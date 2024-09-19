6460f28abe0ec3f5e9ecf6b3406f430b1c0d4558
pvelandia <valentinavelandiacastro2005@gmail.com>
2024-09-19 09:45:55 -0500

    Clase Users 19/09/2024

---
+++ Controllers/UsersController.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class UsersController
+        Inherits Controller
+
+        ' GET: Users
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Users/Details/5
+        Function Details(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' GET: Users/Create
+        Function Create() As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Users/Create
+        <HttpPost()>
+        Function Create(ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add insert logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Users/Edit/5
+        Function Edit(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Users/Edit/5
+        <HttpPost()>
+        Function Edit(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add update logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+
+        ' GET: Users/Delete/5
+        Function Delete(ByVal id As Integer) As ActionResult
+            Return View()
+        End Function
+
+        ' POST: Users/Delete/5
+        <HttpPost()>
+        Function Delete(ByVal id As Integer, ByVal collection As FormCollection) As ActionResult
+            Try
+                ' TODO: Add delete logic here
+
+                Return RedirectToAction("Index")
+            Catch
+                Return View()
+            End Try
+        End Function
+    End Class
+End Namespace
